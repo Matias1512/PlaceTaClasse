098dc6535de867a1c041ddf0814058d4be14a061
Lacaze Yon <lacaze.yon@iutbayonne.univ-pau.fr>
2022-04-01 12:37:25 +0200

    début de la création du pdf du plan de placement

--- PlaceTaClasse/src/Controller/PlanDePlacementController.php
+++ PlaceTaClasse/src/Controller/PlanDePlacementController.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace App\PlanDePlacement;
+namespace App\Controller;
 
 use App\Entity\Placement;
+use App\Entity\Controle;
+use App\Entity\Place;
+use App\Entity\Salle;
 use App\Form\PlacementType;
 use App\Repository\PlacementRepository;
+use App\Repository\PlaceRepository;
 use App\Repository\ControleRepository;
+use App\Repository\SalleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,38 +19,37 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/planDePlacement')]
 class PlanDePlacementController extends AbstractController
 {
-    #[Route('/controleSameTime', name: 'app_plandeplacement_new', methods: ['GET'])]
-    public function new(Request $request, Controle $controle, ControleRepository $controleRepository): Response
+    #[Route('/telecharger/{id}', name: 'app_plandeplacement_telecharger', methods: ['GET'])]
+    public function telecharger(Request $request, Controle $controle, ControleRepository $controleRepository, PlaceRepository $placeRepository, SalleRepository $salleRepository): Response
     {
-        $tabControleSD = array(); //tab controle en meme temps
-        $controlesSD = $controleRepository->findControleSameDate($controle->Date);
         
-        $heure = $controle->HoraireTTDebut;
-        $heures = explode(":", $heure);
-        $heure = intval($heures[0]);
+        
+        //récupérer le  placement
+        $placement = $controle->getPlacement()->toArray();
+        $places = array($placeRepository->findById($placement[0]->getPlace()));
+        for($i = 1; $i < count($placement);  $i++){
+            array_push($places,$placeRepository->findOneById($placement[$i]->getPlace()));
+        }
+        
 
-        if($heure >= 8 && $heure <= 12){
-            foreach($controlesSD as $c){
-                $heure = $c->HoraireTTDebut;
-                $heures = explode(":", $heure);
-                $heure = intval($heures[0]);
-                if($heure >= 8 && $heure <= 12){
-                    array_push($tabControleSD, $c);
-                }
+        //récupérer les salles
+        $salles = array($salleRepository->findById($places[0][0]->getSalle()));
+        for($i = 1; $i < count($places);  $i++){
+            if(!in_array($placeRepository->findById($places[$i]->getSalle()),$salles)){
+                array_push($salles,$salleRepository->findById($places[$i]->getSalle()));
             }
         }
-        else{
-            foreach($controlesSD as $c){
-                $heure = $c->HoraireTTDebut;
-                $heures = explode(":", $heure);
-                $heure = intval($heures[0]);
-                if($heure >= 14){
-                    array_push($tabControleSD, $c);
-                }
-            }
+
+        //écriture du fichier testPDF.html
+        $contenu="<h1>BUT INFORMATIQUE</h1>";
+        for ($i = 0; $i < count($salles);  $i++){
+            $contenu .= '<img src="'.$salles[$i][0]->getPlan().'" alt="image de la salle">';
         }
-        return $this->render('planDePlacement/controleSameTime.html.twig', ['tabControleSD'=>$tabControleSD]);
         
+        file_put_contents('../templates/testPDF.html', $contenu);
+
+        //téléchargement du fichier au format pdf
+        return $this->redirectToRoute('testPDF');
     }
 }
 
